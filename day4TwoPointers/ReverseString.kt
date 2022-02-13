630c48fba75b12c3010041d50fa792e97d061cf2
jestorDev <torrele.09@gmail.com>
2022-02-13 18:33:53 -0500

    Day 4 algo complete

--- day4TwoPointers/ReverseString.kt
+++ day4TwoPointers/ReverseString.kt
@@ -1,10 +1,11 @@
 class Solution8 {
     
-    
-    
     fun reverseString(s: CharArray): Unit {
-        
-        var tmp :Char
+        var str :String = "hola"
+
+            
+        var tmp :Char = str[0]
+        print (tmp)
         if (s.size <= 1 )
             return 
         for (i in 0 .. (s.size/2)-1 ){
---
+++ day4TwoPointers/ReverseWordsInaStringIII.kt
@@ -0,0 +1,35 @@
+
+
+class Solution9 {
+    fun reverseWords(s: String): String {
+        var i_sta: Int = 0
+        var i_end :Int = 0
+        var spaces :Int = 0
+        var ans =  StringBuilder()
+        while (i_sta < s.length){
+            
+            while (i_end < s.length && s [i_end] != ' ')
+                i_end ++
+            
+            if (i_sta < i_end )
+                ans.append( s.substring(i_sta ,i_end).reversed())
+
+            i_sta = i_end
+            while (i_end < s.length &&  s [i_end  ] == ' '){
+                i_end ++
+            }
+            if (i_sta < i_end)
+            ans.append( " ".repeat(i_end - i_sta ))
+            i_sta = i_end
+        }
+        
+        return ans.toString()
+        
+    }
+}
+
+
+fun main(args: Array<String>) {
+    var sol = Solution9()
+    print(sol.reverseWords("Hola a todos"))
+}
